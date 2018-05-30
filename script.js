9c7d6696182b3fb3e5839ccb752f159e63e0505e
Stepan <canal6597@gmail.com>
2018-05-30 16:05:28 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var money = +prompt ('Какой ваш бюджет на месяц?', '60000');
+var name = prompt ('Название вашего магазина?');
+
+mainList = {
+	money,
+	name,
+	open,
+    shopGoods: [product,product2,product3],
+	employers: {}
+};
+
+var product = prompt('Какой тип товаров будем продавать?');
+var product2 = prompt('Какой тип товаров будем продавать?');
+var product3 = prompt('Какой тип товаров будем продавать?');
+
+prompt ('Бюджет на 1 день', money / 30);
